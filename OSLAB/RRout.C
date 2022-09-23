a91016dddb87dfdd02310f2375b51a5aa9f7cb9b
204G1A0563 <204G1A0552@srit.ac.in>
2022-09-23 13:53:22 +0530

    my RR.c was completed

---
+++ OSLAB/RRout.C
@@ -0,0 +1,15 @@
+<pre><font color="#12488B"><b>/204G1A0563/CNOSLAB/OSLAB</b></font>$ ./a.out
+Enter the no of processes -- 2
+
+Enter Burst Time for process 1 -- 3
+
+Enter Burst Time for process 2 -- 4
+
+Enter the size of time slice -- 2
+
+The Average Turnaround time is -- 6.000000
+The Average Waiting time is -- 2.500000 
+	PROCESS	 BURST TIME 	 WAITING TIME	TURNAROUND TIME
+	1 	 3 		 2 		 5 
+	2 	 4 		 3 		 7 
+</pre>
