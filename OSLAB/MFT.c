2efdcd75dae9a2ee5e95c9f35eb6a8b1cbf8a111
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-10-11 11:47:25 +0530

    B.Nandini Created MFT.c

---
+++ OSLAB/MFT.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Nandini
+   Roll No: 204G1A0563
+   Title of the Experiment: CPU Scheduling Using MFT Algorithm
+   Date of Creation: 11-01-2022
+   Date of Execution: 11-10-2022
+*/
