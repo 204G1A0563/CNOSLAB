56eb869d1083486461901507f214b6587de101e3
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-10-11 11:04:35 +0530

    B.Nandini Created PC.c

---
+++ OSLAB/PC.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Nandini
+   Roll No: 204G1A0563
+   Title of the Experiment: CPU Scheduling Using PC Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
