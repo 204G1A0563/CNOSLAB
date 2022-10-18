0a5058aa517050175c7c7d0798208f11a5f49a16
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-10-18 13:07:11 +0530

    B.Nandini Created BF.c

---
+++ OSLAB/BF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Nandini
+   Roll No: 204G1A0563
+   Title of the Experiment: CPU Scheduling Using BF Algorithm
+   Date of Creation: 18-10-2022
+   Date of Execution: 18-10-2022
+*/
