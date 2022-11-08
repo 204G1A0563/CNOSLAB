e8053542c6c98e01598c5c1f98990b3aaa8b6560
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-11-08 12:29:29 +0530

    B.Nandini Created DPP.c

---
+++ OSLAB/DPP.c
@@ -0,0 +1,6 @@
+* Name Of the Candidate: B.Nandini
+   Roll No: 204G1A0563
+   Title of the Experiment: Deadlock Prevention Program
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
