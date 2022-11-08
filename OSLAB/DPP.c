a47762bea5943375621e42ab59d77b7849664284
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-11-08 12:42:35 +0530

    Update DPP.c

--- OSLAB/DPP.c
+++ OSLAB/DPP.c
@@ -1,9 +1,10 @@
-* Name Of the Candidate: B.Nandini
+/* Name Of the Candidate: B.Nandini
    Roll No: 204G1A0563
    Title of the Experiment: Deadlock Prevention Program
    Date of Creation: 08-11-2022
    Date of Execution: 08-11-2022
 */
+
 #include<stdio.h>
 #include<conio.h>
 
