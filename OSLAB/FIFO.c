dab8140c5d1136a175aad77f3e1492cf36810631
204G1A0563 <204G1A0563@srit.ac.in>
2022-11-29 12:28:30 +0530

    FIFO execution Output.

--- OSLAB/FIFO.c
+++ OSLAB/FIFO.c
@@ -7,7 +7,7 @@
 #include<stdio.h>
 //#include<conio.h> 
 int fr[3];
-int main()
+void main()
 {
 void display();
 int i,j,page[12]={2,3,2,1,5,2,4,5,3,2,5,2};
@@ -20,11 +20,13 @@ fr[i]=-1;
 }
 for(j=0;j<12;j++)
 {
-flag1=0; flag2=0; for(i=0;i<12;i++)
+flag1=0; flag2=0; 
+for(i=0;i<12;i++)
 {
 if(fr[i]==page[j])
 {
-flag1=1; flag2=1; break;
+flag1=1; flag2=1; 
+break;
 }
 }
 if(flag1==0)
@@ -33,7 +35,8 @@ for(i=0;i<frsize;i++)
 {
 if(fr[i]==-1)
 {
-fr[i]=page[j]; flag2=1; break;
+fr[i]=page[j]; flag2=1;
+break;
 }
 }
 }
@@ -54,5 +57,4 @@ void display()
 int i; printf("\n");
 for(i=0;i<3;i++)
 printf("%d\t",fr[i]);
-return ;
 }
