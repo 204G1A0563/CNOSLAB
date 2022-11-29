8099bd66f935d9ce6c1454f021016829fa3eb772
204G1A0563 <94587813+204G1A0563@users.noreply.github.com>
2022-11-29 12:58:11 +0530

    B.Nandini Created LRU.c

---
+++ OSLAB/LRU.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Nandini
+   Roll No: 204G1A0563
+   Title of the Experiment: CPU Scheduling Using LRU Algorithm
+   Date of Creation: 29-11-2022
+   Date of Execution: 29-11-2022
+*/
